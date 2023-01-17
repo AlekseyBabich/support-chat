9bd401f3179fdfd39535f55c38c2dc2edaf3319d
AlekseyBabich <babichalex1986@gmail.com>
2023-01-17 03:29:45 -0500

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import {Inter} from '@next/font/google'
+
+const inter = Inter({subsets: ['latin']})
+
+export default function Home() {
+    return (
+
+        <div>
+            <h1>Здесь будет наш чат</h1>
+        </div>
+
+
+    )
+
+}
