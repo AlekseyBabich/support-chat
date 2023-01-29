b7e717bcf9756d6874f08243f47bacf02c8dd3c5
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-01-29 19:55:47 +0600

    feat: add get token endpoint

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -12,6 +12,20 @@ router.get("/", async(ctx, next) => {
     await next();
 })
 
+router.get("/token", async(ctx) => {
+    const authLoginLinkId = ctx.request.query.authLoginLinkId;
+    if(authLoginLinkId){
+        ctx.body  = ({
+            'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+            'refreshToken': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
+            'authLoginLinkId': authLoginLinkId,
+        })
+        return;
+    }
+    ctx.body = ({ 'body': 'no authLoginLinkId' });
+    return;
+})
+
 app.use(json());
 app.use(logger());
 
