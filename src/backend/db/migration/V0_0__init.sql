087d0d3467d7a9968325e000299c2581eec5e926
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-02-05 21:17:38 +0600

    feat: add migration flyway

---
+++ src/backend/db/migration/V0_0__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS system_property(
+    system_property_key TEXT PRIMARY KEY,
+    system_property_value TEXT
+);
+
+
--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
 function config() {
     return {
         CORS: true,
-        baseApiURL: 'http://localhost:5100'
+        baseApiURL: 'http://localhost:5100',
+        db: {
+            host: 'localhost',
+            port: 5432,
+            user: 'postgres',
+            password: 'postgres',
+            database: 'chat',
+        }
     }
 }
 
