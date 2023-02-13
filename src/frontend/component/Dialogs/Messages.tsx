c290c74130e83146394f977bc967f1913586f014
AlekseyBabich <babichalex1986@gmail.com>
2023-02-13 19:53:20 +0600

    add: send message

--- src/frontend/component/Dialogs/Messages.tsx
+++ src/frontend/component/Dialogs/Messages.tsx
@@ -2,27 +2,34 @@ import React, { useState } from 'react'
 import { Button, Grid, Paper, TextField } from '@mui/material'
 import { Box } from '@mui/system'
 
-
+export interface IMessage {
+  id: number
+  text: string
+}
 
 
 const Messages = () => {
 
-  const message: string = 'Тут будут сообщения'
+  const [message, setMessage] = useState([])
+  const [text, setText] = useState('')
 
-
-  /*
-    const [message, setMessage] = useState('')
-  */
+  const sendMessage = () => {
+    const newMessage: IMessage = {
+      id: Date.now(),
+      text
+    }
+    setMessage([...message, newMessage])
+    setText('')
+  }
 
   return (
     <Grid item md={ 9 }>
       <Paper elevation={ 3 }
              sx={ { height: '600px' } }
-
       >
         <Box sx={ { height: '600px', p: '8px'} }>
-          <Box>
-            { message }
+          <Box sx={{ p: '10px' }}>
+            { message.map(el => <Paper elevation={ 3 } sx={{ p: '10px' }}>{el.text}</Paper>) }
           </Box>
         </Box>
         <Box>
@@ -32,18 +39,14 @@ const Messages = () => {
             placeholder='Placeholder'
             multiline
             fullWidth
-
-            /*
-                      onChange={e => e.target.value}
-            */
+            value={text}
+            onChange={e => setText(e.target.value)}
           />
         </Box>
         <Box>
           <Button variant='contained'
                   sx={{ mt: '10px' }}
-            /*
-                            onClick={() => setMessage()}
-            */
+                  onClick={sendMessage}
           >Отправить</Button>
         </Box>
       </Paper>
