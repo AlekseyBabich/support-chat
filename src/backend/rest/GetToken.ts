e386db81c62fb846ac53e572d7f8a80672c496c0
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-02-20 00:37:56 +0600

    fix: edit get tokens

--- src/backend/rest/GetToken.ts
+++ src/backend/rest/GetToken.ts
@@ -14,33 +14,57 @@ export function GetToken(){
     return async (ctx: ExtendableContext) => {
         const authLoginLinkId: string = ctx.request.query.authLoginLinkId as string;
         if(!authLoginLinkId){
-            ctx.body = ({ 'body': 'no authLoginLinkId' });
+            ctx.body = ({
+                body: 'no authLoginLinkId',
+                status_code: ctx.status
+            });
             return;
         }
 
         await db.withTransaction( async (con) => {
             const authLoginLink = await authLoginLinks.selectById(con, authLoginLinkId)
             if(!authLoginLink){
-                ctx.body = 'no authLoginLink'
-                return
+                ctx.body = {
+                    body: 'no authLoginLink',
+                    status_code: ctx.status
+                }
+                return 'error'
             }
 
-            const user  = await supabase.auth.admin.getUserById(authLoginLink.userId)
+            if(authLoginLink.expireAt < new Date()){
+                ctx.body = {
+                    body: 'link expired',
+                    status_code: ctx.status
+                }
+                return 'error'
+            }
 
-            const email: string = user.data.user?.email as string
-            const leftBorder = email.indexOf('-')
-            const rightBorder = email.indexOf('@')
-            const uniqueNumber = email.slice(leftBorder + 1, rightBorder)
+            const response = await fetch(
+              new URL('/auth/v1/token?grant_type=password', backend.db.supabaseUrl),
+              {
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'apikey': backend.db.serviseRoleKey
+                  },
+                  keepalive: false,
+                  method: 'POST',
+                  body: JSON.stringify({ email:  `user-${authLoginLink.userId}@email.com`, password: `user-${authLoginLink.userId}pas` })
+              }
+            )
 
-            const { data, error } = await supabase.auth.signInWithPassword({
-                email: email,
-                password: `user-${uniqueNumber}pas`,
-            })
-
+            const data = await response.json()
+            if (response.status != 200)
+                ctx.body = { body: 'authenticationError'}
             ctx.body = {
-                accessToken: data.session?.access_token,
-                refreshToken: data.session?.refresh_token
+                accessToken: data.access_token,
+                refreshToken: data.refresh_token
             }
+
+            await supabase
+              .from('AuthLoginLinks')
+              .update({ activatedAt: new Date()})
+              .eq('id', authLoginLinkId)
+
             return 'ok'
         })
         return;
--- src/backend/rest/SignUp.ts
+++ src/backend/rest/SignUp.ts
@@ -4,6 +4,7 @@ import backend from "@config/backend";
 import {makeDB} from "@db";
 import {makeUsers} from "@db/repo";
 import { createClient } from '@supabase/supabase-js';
+import {v4 as uuid} from "@lukeed/uuid/secure";
 
 const supabase = createClient(backend.db.supabaseUrl, backend.db.serviseRoleKey)
 const dbPool = new Pool(backend.db)
@@ -13,14 +14,17 @@ const users = makeUsers(db)
 export function SignUp(){
     return async (ctx: ExtendableContext) => {
         await db.withTransaction( async (con) => {
-            const uniqueNumber = Math.floor(Math.random() * 100000)
-            const { data } = await supabase.auth.signUp({
-                email: `user-${uniqueNumber}@email.com`,
-                password: `user-${uniqueNumber}pas`,
+            const userId = uuid()
+
+            await supabase.auth.admin.createUser({
+                email: `user-${userId}@email.com`,
+                password: `user-${userId}pas`,
+                user_metadata: { chat_user: `user:${userId}` },
+                email_confirm: true,
             })
 
             ctx.body = await users.insert(con, {
-                id: String(data.user?.id),
+                id: userId,
                 createdAt: new Date()
             })
             return 'ok'
