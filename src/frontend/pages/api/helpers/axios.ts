0d8d96a87b0461997f6b30313be43d0adda5607a
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-03-16 19:09:22 +0600

    fix: instance axios

--- src/frontend/pages/api/helpers/axios.ts
+++ src/frontend/pages/api/helpers/axios.ts
@@ -1,11 +1,18 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import store from "@src/frontend/store";
 
 const instance = axios.create({
   baseURL: 'http://localhost:5100',
-  headers: {
-    Authorization: `Bearer ${store.getState().auth.token}`
-  }
 });
+
+instance.interceptors.request.use(setTokenToHeaders as any)
+
+function setTokenToHeaders(config: AxiosRequestConfig) {
+  const state = store.getState()
+  if (state.auth.token && config && config.headers) {
+    config.headers.Authorization = 'Bearer ' + state.auth.token
+  }
+  return config
+}
 
 export default instance;
