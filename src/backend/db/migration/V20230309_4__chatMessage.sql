d11df4756566e5db29211c71e1d582dd178732f4
Stephan-Neo <stepan.kazancev.04@bk.ru>
2023-04-12 20:01:31 +0600

    fix: change policy for ChatMessages table

--- src/backend/db/migration/V20230309_4__chatMessage.sql
+++ src/backend/db/migration/V20230309_4__chatMessage.sql
@@ -8,26 +8,25 @@ create table if not exists "ChatMessages" (
 );
 
 create or replace function auth.participant_id()
-    returns text
+    returns text[]
 as $$
 declare
     chat_user_id text;
-    chat_id text;
+    chat_ids text[];
 begin
     select current_setting('request.jwt.claims', true)::jsonb->'user_metadata'->>'chat_user_id'
     into chat_user_id;
 
-    select "chatId" from "ChatUsers"
-        where "userId" = chat_user_id
-        into chat_id;
-
-    return chat_id;
+    select array_agg("chatId") from "ChatUsers"
+    where "userId" = chat_user_id
+    into chat_ids;
+    return chat_ids;
 end
 $$ language plpgsql stable;
 
 alter table "ChatMessages" enable row level security;
 create policy "Allow user read access" on public."ChatMessages" for select
-    using (auth.participant_id() = "chatId" );
+    using(auth.participant_id() @> array["chatId"]);
 grant all on table "ChatMessages" to anon, authenticated, service_role, postgres;
 drop publication if exists supabase_realtime;
 create publication supabase_realtime;
